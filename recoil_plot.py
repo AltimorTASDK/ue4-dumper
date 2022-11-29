7c168aafc7df2228a1e3335d6117edad88859728
AltimorTASDK <altimormc@gmail.com>
2022-11-29 16:25:45 -0500

    make recoil plotter output an image

--- recoil_plot.py
+++ recoil_plot.py
@@ -1,11 +1,19 @@
 import json
 import math
+import numpy as np
+import os
 import sys
 import matplotlib.pyplot as plt
-from itertools import pairwise, tee
+from itertools import pairwise
 from numpy import float32
+from numpy.linalg import norm
 
-SCALE = 180
+RESOLUTION_X = 1920
+RESOLUTION_Y = 1080
+VIEWPORT = [-RESOLUTION_X/2, RESOLUTION_X/2, -RESOLUTION_Y/2, RESOLUTION_Y/2]
+
+HALF_VFOV_TAN = math.tan(45 * math.pi/180) * 3/4
+
 ONE_THIRD = float32(1) / float32(3)
 
 class JsonHook(dict):
@@ -14,11 +22,25 @@ class JsonHook(dict):
             case float(f): return float32(f)
             case value:    return value
 
+def get_output_path(path):
+    """Generate an output path."""
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    guns_dir = os.path.join(script_dir, "guns")
+    path = os.path.realpath(path)
+
+    if os.path.commonpath([guns_dir, os.path.realpath(path)]) == guns_dir:
+        subpath = os.path.relpath(path, guns_dir)
+    else:
+        subpath = os.path.basename(path)
+
+    return os.path.join(script_dir, "recoil",
+                        f"{os.path.splitext(subpath)[0]}.png")
+
 def lerp(a, b, t):
     return a + (b - a) * t
 
-def bezier(P0, *PN, t):
-    return bezier(*(lerp(*p,t) for p in pairwise([P0,*PN])), t=t) if PN else P0
+def bezier(a, *b, t):
+    return bezier(*(lerp(*p, t) for p in pairwise([a, *b])), t=t) if b else a
 
 def eval_curve(curve, time):
     time = float32(time)
@@ -44,26 +66,26 @@ def eval_curve(curve, time):
                           key2.Value,
                           t=alpha)
 
-def round32(f):
-    return next((r for r in (round(float(f), n) for n in range(8))
-                         if float32(f) == float32(r)), f)
-
 def smooth_step(a, b, t):
     return lerp(a, b, (3 - 2*t) * t*t)
 
-def get_spray_pattern(gun, start=0, end=1000, *, flip=False):
+def get_spray_pattern(gun, start=0, end=1000, *, flip=False, subdivs=1):
     stability = gun.Stability
     yaw_manipulator = stability.YawDirectionManipulator
     flip_time = 0.0
 
-    for bullet in range(int(start), min(int(end), gun.MagazineAmmo.MaxAmmo)):
-        pitch = eval_curve(stability.PitchRecoil.FiringCurve, bullet)
-        yaw   = eval_curve(stability.YawRecoil.FiringCurve,   bullet)
+    start = int(start)
+    end   = min(int(end), gun.MagazineAmmo.MaxAmmo)
+
+    for n in range(start * subdivs, (end - 1) * subdivs + 1):
+        bullet = n / subdivs
+        pitch  = eval_curve(stability.PitchRecoil.FiringCurve, bullet)
+        yaw    = eval_curve(stability.YawRecoil.FiringCurve,   bullet)
 
         if flip and bullet >= yaw_manipulator.ProtectedBulletCount:
             fraction = flip_time / yaw_manipulator.TimeToSwitchYaw
             yaw *= smooth_step(1, -1, min(fraction, 1))
-            flip_time += 1 / gun.FiringState.FiringRate
+            flip_time += 1 / gun.FiringState.FiringRate / subdivs
 
         yield (pitch, yaw)
 
@@ -73,31 +95,60 @@ def deg_to_mil(degrees):
 def spray_to_mil(pattern):
     return [(deg_to_mil(yaw), deg_to_mil(pitch)) for pitch, yaw in pattern]
 
+def deg_to_px(degrees):
+    return math.tan(degrees * math.pi/180) / HALF_VFOV_TAN * RESOLUTION_Y/2
+
+def spray_to_px(pattern):
+    return [(deg_to_px(yaw), deg_to_px(pitch)) for pitch, yaw in pattern]
+
+def invert_axes(points):
+    return [[-x for x in p] for p in points]
+
 def split_axes(points):
     return [[p[n] for p in points] for n in range(len(points[0]))]
 
 def main():
-    with open(sys.argv[1], "r") as file:
+    in_path = sys.argv[1]
+
+    with open(in_path, "r") as file:
         gun = json.load(file, object_hook=lambda obj: JsonHook(obj))
 
-    for bullet, (pitch, yaw) in enumerate(get_spray_pattern(gun)):
-        print(f"{bullet:2d} recoil ({round32(pitch):.7f}, {round32(yaw):.7f})")
+    plt.rc('lines', linewidth=0.5, markersize=1, markeredgewidth=0)
 
-    pattern = spray_to_mil(get_spray_pattern(gun))
-    flipped = spray_to_mil(get_spray_pattern(gun, flip=True))
+    plt.axis('off')
+    plt.subplots_adjust(0, 0, 1, 1, 0, 0)
 
-    pattern_highest = sorted(pattern, key=lambda p: p[1])[-1]
-    flipped_highest = sorted(flipped, key=lambda p: p[1])[-1]
-    pattern_x, pattern_y = split_axes([*pattern, pattern_highest])
-    flipped_x, flipped_y = split_axes([*flipped, flipped_highest])
-    plt.fill_betweenx(pattern_y, pattern_x, flipped_x, alpha=0.5)
+    plt.imshow(plt.imread("recoil_bg.png"), extent=VIEWPORT)
+    plt.axhspan(*VIEWPORT[2:4], *VIEWPORT[0:2], color='0', alpha=0.8)
 
-    plt.plot(flipped_x[:-1], flipped_y[:-1], "-o")
-    plt.plot(pattern_x[:-1], pattern_y[:-1], "-o")
+    pattern = spray_to_px(get_spray_pattern(gun, subdivs=10))
+    flipped = spray_to_px(get_spray_pattern(gun, subdivs=10, flip=True))
+    pattern_padded = [*pattern, sorted(pattern, key=lambda p: p[1])[-1]]
+    flipped_padded = [*flipped, sorted(flipped, key=lambda p: p[1])[-1]]
+    pattern_x, pattern_y = split_axes(invert_axes(pattern_padded))
+    flipped_x, flipped_y = split_axes(invert_axes(flipped_padded))
+    plt.fill_betweenx(pattern_y, pattern_x, flipped_x, facecolor='1', alpha=0.25)
 
-    plt.axis([-SCALE / 2, SCALE / 2, 0, SCALE])
-    plt.gca().set_aspect('equal')
-    plt.show()
+    flip_start = int(gun.Stability.YawDirectionManipulator.ProtectedBulletCount)
+
+    pattern = spray_to_px(get_spray_pattern(gun, subdivs=10))
+    flipped = spray_to_px(get_spray_pattern(gun, flip_start, subdivs=10, flip=True))
+    plt.plot(*split_axes(invert_axes(flipped)), markerfacecolor='#13AFC0')
+    plt.plot(*split_axes(invert_axes(pattern)), markerfacecolor='#FF7F00')
+
+    pattern = spray_to_px(get_spray_pattern(gun))
+    flipped = spray_to_px(get_spray_pattern(gun, flip_start, flip=True))
+    plt.plot(*split_axes(invert_axes(flipped)), "o", markerfacecolor='#13AFC0')
+    plt.plot(*split_axes(invert_axes(pattern)), "o", markerfacecolor='#FF7F00')
+
+    plt.autoscale()
+    plt.gca().set_xbound(-256, 256)
+    plt.gca().set_ybound(-256, 256)
+
+    out_path = get_output_path(in_path)
+    os.makedirs(os.path.dirname(out_path), exist_ok=True)
+    plt.savefig(out_path, dpi=300, bbox_inches='tight', pad_inches=0)
+    print(f"Wrote to \"{out_path}\"")
 
 if __name__ == "__main__":
     main()
