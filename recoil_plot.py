cb170c8408f21f8e3fb81bfb6fd4d251fbb9123a
AltimorTASDK <altimormc@gmail.com>
2022-11-30 21:10:10 -0500

    cleanup, halve recoil for offset from crosshair only

--- recoil_plot.py
+++ recoil_plot.py
@@ -1,19 +1,18 @@
 import json
 import math
-import numpy as np
 import os
 import sys
 import matplotlib.pyplot as plt
 from itertools import pairwise
 from matplotlib.transforms import Bbox
-from numpy import float32
-from numpy.linalg import norm
+from numpy import array, float32, linalg
 
 VIEWPORT_X = 1920
 VIEWPORT_Y = 1080
 VIEWPORT   = [-VIEWPORT_X/2, VIEWPORT_X/2, -VIEWPORT_Y/2, VIEWPORT_Y/2]
-CROP       = np.array([256, 256])
-DPI        = 300
+ZOOM       = 6
+CROP       = array([256, 256]) * ZOOM
+DPI        = 300 * ZOOM
 
 HALF_VFOV_TAN = math.tan(45 * math.pi/180) * 3/4
 
@@ -72,13 +71,25 @@ def eval_curve(curve, time):
 def smooth_step(a, b, t):
     return lerp(a, b, (3 - 2*t) * t*t)
 
-def get_pattern(gun, start=0, end=1000, *, flip=False, subdivs=1):
-    stability = gun.Stability
-    yaw_manipulator = stability.YawDirectionManipulator
-    flip_time = 0.0
+def deg_to_mil(degrees):
+    return math.tan(degrees * math.pi/180) * 1000
 
-    start = int(start)
-    end   = min(int(end), gun.MagazineAmmo.MaxAmmo)
+def deg_to_px(degrees):
+    return math.tan(degrees * math.pi/180) / HALF_VFOV_TAN * VIEWPORT_Y/2
+
+def get_pattern(gun, start=None, end=None, *, flip=False, subdivs=1,
+                units='px', half=True, invert=True):
+    stability       = gun.Stability
+    yaw_manipulator = stability.YawDirectionManipulator
+    flip_time       = 0.0
+
+    if start is None:
+        start = int(yaw_manipulator.ProtectedBulletCount) if flip else 0
+
+    if end is None:
+        end = int(gun.MagazineAmmo.MaxAmmo)
+
+    pattern = []
 
     for n in range(start * subdivs, (end - 1) * subdivs + 1):
         bullet = n / subdivs
@@ -90,56 +101,55 @@ def get_pattern(gun, start=0, end=1000, *, flip=False, subdivs=1):
             yaw *= smooth_step(1, -1, min(fraction, 1))
             flip_time += 1 / gun.FiringState.FiringRate / subdivs
 
-        yield (pitch, yaw)
+        if invert: pitch, yaw = -pitch,   -yaw
+        if half:   pitch, yaw =  pitch/2,  yaw/2
 
-def deg_to_mil(degrees):
-    return math.tan(degrees * math.pi/180) * 1000
+        match units:
+            case 'deg': pattern.append((pitch, yaw))
+            case 'mil': pattern.append((deg_to_mil(yaw), deg_to_mil(pitch)))
+            case 'px':  pattern.append((deg_to_px (yaw), deg_to_px (pitch)))
+            case _:     raise ValueError
 
-def spray_to_mil(pattern):
-    return [(deg_to_mil(yaw), deg_to_mil(pitch)) for pitch, yaw in pattern]
+    return array(pattern)
 
-def deg_to_px(degrees):
-    return math.tan(degrees * math.pi/180) / HALF_VFOV_TAN * VIEWPORT_Y/2
+def line_intersection(a, b):
+    try:
+        return linalg.solve(array([a[1]-a[0], b[0]-b[1]]).T, b[0]-a[0])
+    except linalg.LinAlgError:
+        return None
 
-def spray_to_px(pattern):
-    return [(deg_to_px(yaw), deg_to_px(pitch)) for pitch, yaw in pattern]
-
-def invert(points):
-    return [[-x for x in p] for p in points]
-
-def split_axes(points):
-    return [[p[n] for p in points] for n in range(len(points[0]))]
+def fix_overlap(points):
+    i = 0
+    result = []
+    while i < len(points) - 1:
+        low_t = 1
+        closest = None
+        for j in range(len(points) - 1):
+            if (t := line_intersection(points[i:i+2], points[j:j+2])):
+                t[0]
 
 def main():
     in_path = sys.argv[1]
 
     with open(in_path, "r") as file:
-        gun = json.load(file, object_hook=lambda obj: JsonHook(obj))
+        gun = json.load(file, object_hook=JsonHook)
 
-    plt.rc('lines', linewidth=0.5, markersize=1, markeredgewidth=0)
+    plt.rc('lines', linewidth=0.5/ZOOM, markersize=1/ZOOM, markeredgewidth=0)
 
     plt.imshow(plt.imread("recoil_bg.png"), extent=VIEWPORT)
     plt.axhspan(*VIEWPORT[2:4], *VIEWPORT[0:2], color='0', alpha=0.75)
 
-    spray = spray_to_px(get_pattern(gun, subdivs=10))
-    flip  = spray_to_px(get_pattern(gun, subdivs=10, flip=True))
-    spray_closed = [*spray, sorted(spray, key=lambda p: p[1])[-1]]
-    flip_closed  = [*flip,  sorted(flip,  key=lambda p: p[1])[-1]]
-    spray_x, spray_y = split_axes(invert(spray_closed))
-    flip_x,  flip_y  = split_axes(invert(flip_closed))
-    plt.fill_betweenx(spray_y, spray_x, flip_x, facecolor='1', alpha=0.25)
+    spray = get_pattern(gun, 0, subdivs=10, flip=False)
+    flip  = get_pattern(gun, 0, subdivs=10, flip=True )
+    spray = array([*spray, spray[spray[:,1].argsort()[0]]]).T
+    flip  = array([*flip,  flip [flip [:,1].argsort()[0]]]).T
+    plt.fill_betweenx(spray[1], spray[0], flip[0], facecolor='1', alpha=0.25)
 
-    flip_start = int(gun.Stability.YawDirectionManipulator.ProtectedBulletCount)
+    plt.plot(*get_pattern(gun, subdivs=10, flip=True ).T, color='#13AFC07F')
+    plt.plot(*get_pattern(gun, subdivs=10, flip=False).T, color='#FF7F007F')
 
-    spray = spray_to_px(get_pattern(gun, subdivs=10))
-    flip  = spray_to_px(get_pattern(gun, flip_start, subdivs=10, flip=True))
-    plt.plot(*split_axes(invert(flip)),  color='#13AFC0', alpha=0.5)
-    plt.plot(*split_axes(invert(spray)), color='#FF7F00', alpha=0.5)
-
-    spray = spray_to_px(get_pattern(gun))
-    flip  = spray_to_px(get_pattern(gun, flip_start, flip=True))
-    plt.plot(*split_axes(invert(flip)),  "o", color='#13AFC0')
-    plt.plot(*split_axes(invert(spray)), "o", color='#FF7F00')
+    plt.plot(*get_pattern(gun, flip=True ).T, "o", color='#13AFC0')
+    plt.plot(*get_pattern(gun, flip=False).T, "o", color='#FF7F00')
 
     plt.axis('off')
     plt.subplots_adjust(0, 0, 1, 1)
