70c8ea306f7aad53c495e91742cb9c306be4869b
AltimorTASDK <altimormc@gmail.com>
2023-01-26 01:16:40 -0500

    support or skip various movie types used in UI

--- ue4/structs/types.py
+++ ue4/structs/types.py
@@ -4,6 +4,8 @@ from .vector import FVector, FVector2D, FVector4, FIntPoint
 from .quat import FQuat
 from .color import FColor, FLinearColor
 from .gameplaytagcontainer import FGameplayTagContainer
+from .moviesceneframerange import FMovieSceneFrameRange
+from .framenumber import FFrameNumber
 from ue4 import FGuid
 
 STRUCT_TYPE_MAP = {
@@ -18,5 +20,17 @@ STRUCT_TYPE_MAP = {
     "LinearColor": FLinearColor,
     "Guid": FGuid,
     "GameplayTagContainer": FGameplayTagContainer,
+    "MovieSceneFrameRange": FMovieSceneFrameRange,
+    "MovieSceneFloatChannel": None,
+    "MovieSceneEvaluationKey": None,
+    "MovieSceneEvaluationTemplate": None,
+    "MovieSceneSequenceID": None,
+    "MovieSceneTrackIdentifier": None,
+    "MovieSceneEvaluationFieldEntityTree": None,
+    "ColorMaterialInput": None,
+    "DateTime": None,
+    "Timespan": None,
+    "Box2D": None,
+    "FrameNumber": FFrameNumber,
     "KeyHandleMap": lambda r: None
 }
